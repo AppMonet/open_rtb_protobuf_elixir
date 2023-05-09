ef6b80048f083acf2bb843ea1c23fe9d1e5ec444
Nico Piderman <nico.piderman@gmail.com>
2023-05-09 12:05:29 +0200

    moduledoc

--- lib/open_rtb_protobuf.ex
+++ lib/open_rtb_protobuf.ex
@@ -1,4 +1,9 @@
 defmodule OpenRtbProtobuf do
+  @moduledoc """
+  OpenRTB v2.6 Protobuf support for Elixir using [Protox](https://github.com/ahamez/protox).
+
+  Protobuf schemas taken from [Google](https://developers.google.com/authorized-buyers/rtb/downloads/openrtb-proto).
+  """
   use Protox,
     files: [
       "./protobuf/schema.proto"
